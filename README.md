250300c7b361213f189ddeb688f7143a589e790f
maxflex <makcyxa-k@yandex.ru>
2020-03-17 01:14:08 +0300

    Likes counter example added, dependencies updated

--- README.md
+++ README.md
@@ -3,33 +3,41 @@
 </p>
 
 # V-Number
-### Animated number transition library for Vue.js
 
-Simple lightweight library for smooth horizontal number transitions
+## Animated number transition library for Vue.js
 
-[LIVE DEMO](https://maxflex.github.io/v-number/)
+### Simple lightweight library for smooth vertical number transitions
+
+[LIVE DEMO](https://maxflex.github.io/v-number/): animated likes counter
 
 <img src="https://raw.githubusercontent.com/maxflex/v-number/master/docs/img/preview.gif">
 
 ## Installation
+
 ```
 yarn add @maxflex/v-number
 // or
 npm install @maxflex/v-number
 ```
+
 ## Usage
+
 Just import:
+
 ```js
-import { VNumber } from '@maxflex/v-number'
+import { VNumber } from "@maxflex/v-number";
 ```
 
 And use:
+
 ```html
-<v-number v-model='someNumber'></v-number>
+<v-number v-model="someNumber"></v-number>
 ```
 
 ## Customization
+
 You can set custom animation speed in ms:
+
 ```html
-<v-number :speed='2000' v-model='someNumber'></v-number>
+<v-number :speed="2000" v-model="someNumber"></v-number>
 ```
--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,29 @@
 <template>
-  <div class='app'>
+  <div class="app">
     <div>
-      <div class='header'>
-        <img class='header__logo' src='img/logo.png'/>
-        <h1 class='header__title'>Number</h1>
+      <div class="header">
+        <img class="header__logo" src="img/logo.png" />
+        <h1 class="header__title">Number</h1>
       </div>
-      <h2 style='text-align: center'>
-        Animated number transition library for Vue.js
+      <h2 style="text-align: center">
+        Animated number transition & likes counter library for Vue.js<br />
       </h2>
-      <v-number class='v-number' :value='number' />
+      <v-number class="v-number-example" :value="number" />
     </div>
-    <div style='margin-top: 100px'>
-      <p>Usage:</p>
+    <div style="margin-top: 100px">
+      <p>Likes counter animation example:</p>
+      <div class="likes-counter-example">
+        <div class="likes-counter-example__likes">
+          <i class="material-icons">favorite</i>
+          <v-number v-model="likes"></v-number>
+          <span> people liked </span>
+        </div>
+        <div class="likes-counter-example__button" @click="like()">
+          <i class="material-icons">thumb_up</i>
+          <span>Like</span>
+        </div>
+      </div>
+      <p style="margin-top: 60px">Usage:</p>
       <code>
         <div>
           &#60;v-number v-model='someValue'&#62;
@@ -19,15 +31,21 @@
       </code>
       <!-- <input style='margin-top: 15px' type='number' v-model='number' @focus='clearInterval' /> -->
     </div>
-    <div style='flex: 1'></div>
+    <div style="flex: 1"></div>
     <div>
-      <iframe src="https://ghbtns.com/github-btn.html?user=maxflex&repo=v-number&type=star&count=true&size=large" frameborder="0" scrolling="0" width="160px" height="30px"></iframe>
+      <iframe
+        src="https://ghbtns.com/github-btn.html?user=maxflex&repo=v-number&type=star&count=true&size=large"
+        frameborder="0"
+        scrolling="0"
+        width="160px"
+        height="30px"
+      ></iframe>
     </div>
   </div>
 </template>
 
 <script>
-import VNumber from './components/VNumber'
+import VNumber from "./components/VNumber";
 
 export default {
   components: {
@@ -37,40 +55,54 @@ export default {
   data() {
     return {
       number: 1,
+      likes: 153,
+      isLiked: false,
       intervalSpeed: 2000,
-      interval: null,
-    }
+      interval: null
+    };
   },
-  
+
   created() {
-    this.startInterval()
+    this.startInterval();
   },
 
   methods: {
     startInterval() {
-      this.interval = setInterval(() => this.number++, this.intervalSpeed)
+      this.interval = setInterval(() => this.number++, this.intervalSpeed);
     },
 
     clearInterval() {
-      clearInterval(this.interval)
+      clearInterval(this.interval);
     },
+
+    like() {
+      this.likes += 1;
+    }
   }
-}
+};
 </script>
 
-<style lang='scss'>
-html, body {
+<style lang="scss">
+html,
+body {
   height: 100%;
   padding: 0;
   margin: 0;
   overflow: hidden;
-  background: linear-gradient(to bottom, #34495e, #1e647c, #00818f, #009e90, #41b883); 
+  background: linear-gradient(
+    to bottom,
+    #34495e,
+    #1e647c,
+    #00818f,
+    #009e90,
+    #41b883
+  );
   color: white;
-  font-family: 'Open Sans', sans-serif;
+  font-family: "Open Sans", sans-serif;
 }
 
 code {
-  background: rgba(black, .5);
+  background: rgba(black, 0.5);
   width: 460px;
   display: block;
   padding: 20px;
@@ -92,7 +124,7 @@ code {
   }
 }
 
-.v-number {
+.v-number-example {
   font-size: 100px;
   color: PaleVioletRed;
 }
@@ -112,8 +144,52 @@ input {
   border-radius: 4px;
   padding: 10px;
   font-size: 16px;
-  background: rgba(white, .5);
+  background: rgba(white, 0.5);
   outline: none;
   width: 60px;
+}
+
+.likes-counter-example {
+  display: flex;
+  align-items: center;
+  &__likes {
+    display: flex;
+    margin-right: 30px;
+    align-items: center;
+    & .material-icons {
+      margin-right: 3px;
+      color: palevioletred;
+    }
+    & .v-number {
+      margin-right: 3px;
+      color: palevioletred;
+      font-weight: bold;
+    }
+  }
+  &__button {
+    $color: #4267b2;
+    display: flex;
+    background-color: $color;
+    padding: 6px 30px;
+    align-items: center;
+    border-radius: 6px;
+    width: fit-content;
+    cursor: pointer;
+    transition: all ease-in-out 0.2s;
+    &:hover {
+      background-color: darken($color, 6);
+      box-shadow: 8px 8px rgba(black, 0.5);
+      transform: scale(1.04);
+    }
+    &:active {
+      background-color: darken($color, 8);
+      transform: scale(1.01);
+      box-shadow: 2px 2px rgba(black, 0.5);
+    }
+    & .material-icons {
+      margin-right: 5px;
+      font-size: 18px;
+    }
+  }
 }
 </style>
